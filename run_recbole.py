e94915febbcb95bafa1cebe7b856e74a2235f2b3
nnpuyen <nuyencrkh@gmail.com>
2026-03-30 21:52:33 +0700

    train on gowalla

--- run_recbole.py
+++ run_recbole.py
@@ -16,7 +16,7 @@ from recbole.quick_start import run_recbole
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', '-m', type=str, default='DirectAU', help='name of models')
-    parser.add_argument('--dataset', '-d', type=str, default='Beauty', help='name of datasets')
+    parser.add_argument('--dataset', '-d', type=str, default='Yelp', help='name of datasets')
     parser.add_argument('--config_files', type=str, default=None, help='config files')
 
     args, _ = parser.parse_known_args()
